65803c655a6cb5f83826f1ff9c78c529e7ad3def
Muhammad Mustafa <mmustafa.bese20seecs@seecs.edu.pk>
2024-03-12 11:11:19 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Saved Suggestions',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: SavedSuggestionsScreen(),
     );
